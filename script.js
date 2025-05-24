37428727ebabddfc83d29d41e602d60b9dca7823
agmanyadav <agmanyadav49132@gmail.com>
2025-05-24 19:51:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+function bookSession() {
+  alert("Thank you for choosing SoundNest Studio! We'll contact you shortly.");
+}
---
+++ studio-info.cpp
@@ -0,0 +1,10 @@
+// studio-info.cpp
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to SoundNest Studio!" << endl;
+    cout << "We offer recording, mixing, and mastering services." << endl;
+    cout << "Located at 123 Music Lane, Harmony City." << endl;
+    return 0;
+}
